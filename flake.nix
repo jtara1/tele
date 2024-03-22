250e6bace82072dff96bea1a775ce724b9d3f888
James T <github.jtara1@gmail.com>
2024-03-21 18:05:10 -0700

    feat: systemd service to install docker plugin, loki; tested

--- flake.nix
+++ flake.nix
@@ -12,19 +12,19 @@
            # in general, for json-line logs with properties
            labels = "level,msg,ts";
            # for loki
-#           loki-url = "http://localhost:3100/loki/api/v1/push";
-#           loki-batch-size = "400";
-#           loki-pipeline-stages = ''
-#             - json:
-#                 expressions:
-#                   level: level
-#                   msg: msg
-#                   ts: ts
-#             - labels:
-#                 level: level
-#                 msg: msg
-#                 ts: ts
-#           '';
+           loki-url = "http://localhost:3100/loki/api/v1/push";
+           loki-batch-size = "400";
+           loki-pipeline-stages = ''
+             - json:
+                 expressions:
+                   level: level
+                   msg: msg
+                   ts: ts
+             - labels:
+                 level: level
+                 msg: msg
+                 ts: ts
+           '';
          };
        };
     in
@@ -37,13 +37,41 @@
           config.virtualisation.docker.rootless.customSettings or {}
         ];
 
-        system.activationScripts = {
-          dockerRootlessRestart.text = "systemctl --user restart docker";
-          dockerPluginLoki.text = ''
-            if ! docker plugin ls --filter enabled=true | grep -q loki; then
-              docker plugin install grafana/loki-docker-driver:2.9.4 --alias loki --grant-all-permissions
-            fi
-          '';
+        systemd.services = {
+          dockerRootlessRestart = {
+            wantedBy = [ "multi-user.target" ];
+            after = [ "docker.service" ];
+            requires = [ "docker.service" ];
+            description = "Restart docker service after nixos-rebuild switch.";
+            serviceConfig = {
+              Type = "oneshot";
+              ExecStart = ''
+                /bin/sh -c '
+                  if ! systemctl --user is-active --quiet docker; then
+                    echo systemd dockerRootlessRestart is restarting docker
+                    systemctl --user restart docker
+                  fi
+                '
+              '';
+            };
+          };
+
+          dockerPluginLokiInstall = {
+            wantedBy = [ "multi-user.target" ];
+            after = [ "docker.service" ];
+            requires = [ "docker.service" ];
+            description = "Install docker plugin, loki.";
+            serviceConfig = {
+              Type = "oneshot";
+              ExecStart = ''
+                /bin/sh -c '
+                  if ! docker plugin ls --filter enabled=true | grep -q loki; then
+                    docker plugin install grafana/loki-docker-driver:2.9.4 --alias loki --grant-all-permissions
+                  fi
+                '
+              '';
+            };
+          };
         };
       };
     };
