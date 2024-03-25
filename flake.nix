2031337cc391b62156f3efbb654f3b1b130a360f
James T <github.jtara1@gmail.com>
2024-03-24 19:10:29 -0700

    feat: (wip) adding loki and grafana in flake.nix

--- flake.nix
+++ flake.nix
@@ -7,7 +7,39 @@
     nixosModules.default = { config, lib, ... }: {
       options = {};
       config = lib.mkIf (config.virtualisation.docker.rootless.enable) {
-        # enable docker loki plugin in docker daemon.json (settings)
+        # --- run loki
+        services.loki = {
+          enable = true;
+          useLocally = true; # simplification assumption - logs-app runs local to apps
+        };
+
+        # --- run grafana
+        services.grafana = {
+          enable = true;
+          settings = {
+            security = {
+              admin_user = "admin";
+              admin_password = "admin"; # FIXME: parameterize through build args or options
+            };
+          };
+          provision = {
+            enable = true;
+            datasources.apiVersion = 1;
+            datasources.settings = [
+              {
+                name = "Loki";
+                type = "loki";
+                access = "proxy";
+                url = "http://localhost:3100";
+                jsonData.maxLines = 1000;
+                jsonData.tlsSkipVerify = true;
+                isDefault = true;
+              }
+            ];
+          };
+        };
+
+        # --- in docker daemon.json (settings), enable docker loki plugin
         virtualisation.docker.rootless.daemon.settings = {
           log-driver = lib.mkDefault "loki";
           log-opts = {
@@ -30,6 +62,7 @@
           };
         };
 
+        # --- systemd oneshot to install docker loki plugin
         systemd.services = {
           # TODO: This needs more testing. Is docker available before docker.service? but after the service would err out bc daemon.json requires loki plugin
           # the `docker plugin install` should just be done manually until further testing/fix
