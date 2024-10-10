4afabacaacfd1c79f04fe65f638149ba271b5554
James T <github.jtara1@gmail.com>
2024-10-09 19:10:14 -0700

    chore: reverted variables invalid definition

--- flake.nix
+++ flake.nix
@@ -283,14 +283,6 @@
                       }";
                   }
                 ];
-                variables = [
-                  {
-                    name = "job";
-                    type = "label_values";
-                    datasource = "Prometheus";
-                    query = "label_values(job)";
-                  }
-                ];
               };
               dashboards.settings = {
                 apiVersion = 1;
